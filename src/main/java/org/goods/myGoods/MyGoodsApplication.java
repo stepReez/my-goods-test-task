6f3fc0d361bd4457c9f133de495707b1f9f6218c
sReez <step.reznikoff@yandex.ru>
2024-05-09 14:50:12 +0300

    init

---
+++ src/main/java/org/goods/myGoods/MyGoodsApplication.java
@@ -0,0 +1,13 @@
+package org.goods.myGoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/goods/myGoods/MyGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goods.myGoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
